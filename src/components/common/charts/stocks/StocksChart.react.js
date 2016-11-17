87c58c7db8b478abc5b34788c8abb2cf3664c038
newname <new@email>
2016-11-17 13:45:01 +0200

    chart design

--- src/components/common/charts/stocks/StocksChart.react.js
+++ src/components/common/charts/stocks/StocksChart.react.js
@@ -5,209 +5,6 @@ import Highcharts from 'highcharts';
 import FontAwesome from 'react-fontawesome';
 import './StocksChart.css';
 
-console.log('Highcharts', Highcharts);
-
-Highcharts.theme = {
-    colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
-        '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-    chart: {
-        backgroundColor: {
-            linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-            stops: [
-                [0, '#2a2a2b'],
-                [1, '#3e3e40']
-            ]
-        },
-        style: {
-            fontFamily: '\'Unica One\', sans-serif'
-        },
-        plotBorderColor: '#606063'
-    },
-    title: {
-        style: {
-            color: '#E0E0E3',
-            textTransform: 'uppercase',
-            fontSize: '20px'
-        }
-    },
-    subtitle: {
-        style: {
-            color: '#E0E0E3',
-            textTransform: 'uppercase'
-        }
-    },
-    xAxis: {
-        gridLineColor: '#707073',
-        labels: {
-            style: {
-                color: '#E0E0E3'
-            }
-        },
-        lineColor: '#707073',
-        minorGridLineColor: '#505053',
-        tickColor: '#707073',
-        title: {
-            style: {
-                color: '#A0A0A3'
-
-            }
-        }
-    },
-    yAxis: {
-        gridLineColor: '#707073',
-        labels: {
-            style: {
-                color: '#E0E0E3'
-            }
-        },
-        lineColor: '#707073',
-        minorGridLineColor: '#505053',
-        tickColor: '#707073',
-        tickWidth: 1,
-        title: {
-            style: {
-                color: '#A0A0A3'
-            }
-        }
-    },
-    tooltip: {
-        backgroundColor: 'rgba(0, 0, 0, 0.85)',
-        style: {
-            color: '#F0F0F0'
-        }
-    },
-    plotOptions: {
-        series: {
-            dataLabels: {
-                color: '#B0B0B3'
-            },
-            marker: {
-                lineColor: '#333'
-            }
-        },
-        boxplot: {
-            fillColor: '#505053'
-        },
-        candlestick: {
-            lineColor: 'white'
-        },
-        errorbar: {
-            color: 'white'
-        }
-    },
-    legend: {
-        itemStyle: {
-            color: '#E0E0E3'
-        },
-        itemHoverStyle: {
-            color: '#FFF'
-        },
-        itemHiddenStyle: {
-            color: '#606063'
-        }
-    },
-    credits: {
-        style: {
-            color: '#666'
-        }
-    },
-    labels: {
-        style: {
-            color: '#707073'
-        }
-    },
-
-    drilldown: {
-        activeAxisLabelStyle: {
-            color: '#F0F0F3'
-        },
-        activeDataLabelStyle: {
-            color: '#F0F0F3'
-        }
-    },
-
-    navigation: {
-        buttonOptions: {
-            symbolStroke: '#DDDDDD',
-            theme: {
-                fill: '#505053'
-            }
-        }
-    },
-
-    // scroll charts
-    rangeSelector: {
-        buttonTheme: {
-            fill: '#505053',
-            stroke: '#000000',
-            style: {
-                color: '#CCC'
-            },
-            states: {
-                hover: {
-                    fill: '#707073',
-                    stroke: '#000000',
-                    style: {
-                        color: 'white'
-                    }
-                },
-                select: {
-                    fill: '#000003',
-                    stroke: '#000000',
-                    style: {
-                        color: 'white'
-                    }
-                }
-            }
-        },
-        inputBoxBorderColor: '#505053',
-        inputStyle: {
-            backgroundColor: '#333',
-            color: 'silver'
-        },
-        labelStyle: {
-            color: 'silver'
-        }
-    },
-
-    navigator: {
-        handles: {
-            backgroundColor: '#666',
-            borderColor: '#AAA'
-        },
-        outlineColor: '#CCC',
-        maskFill: 'rgba(255,255,255,0.1)',
-        series: {
-            color: '#7798BF',
-            lineColor: '#A6C7ED'
-        },
-        xAxis: {
-            gridLineColor: '#505053'
-        }
-    },
-
-    scrollbar: {
-        barBackgroundColor: '#808083',
-        barBorderColor: '#808083',
-        buttonArrowColor: '#CCC',
-        buttonBackgroundColor: '#606063',
-        buttonBorderColor: '#606063',
-        rifleColor: '#FFF',
-        trackBackgroundColor: '#404043',
-        trackBorderColor: '#404043'
-    },
-
-    // special colors for some of the
-    legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
-    background2: '#505053',
-    dataLabelsColor: '#B0B0B3',
-    textColor: '#C0C0C0',
-    contrastTextColor: '#F0F0F3',
-    maskColor: 'rgba(255,255,255,0.3)'
-};
-
-//Highcharts.setOptions(Highcharts.theme);
-
 export default class StocksChart extends React.Component {
     constructor(props, context) {
         super(props, context);
@@ -216,7 +13,7 @@ export default class StocksChart extends React.Component {
 
         setTimeout(() => {
             this.setState({loading: false});
-        }, 5000);
+        }, 2500);
     }
 
     componentDidMount() {
@@ -228,24 +25,73 @@ export default class StocksChart extends React.Component {
     }
 
     render() {
-        const data = [[1220832000000, 22.56], [1220918400000, 21.67], [1221004800000, 21.66], [1221091200000, 21.81], [1221177600000, 21.28], [1221436800000, 20.05], [1221523200000, 19.98], [1221609600000, 18.26], [1221696000000, 19.16], [1221782400000, 20.13], [1222041600000, 18.72], [1222128000000, 18.12], [1222214400000, 18.39], [1222300800000, 18.85], [1222387200000, 18.32], [1222646400000, 15.04], [1222732800000, 16.24], [1222819200000, 15.59], [1222905600000, 14.3], [1222992000000, 13.87], [1223251200000, 14.02], [1223337600000, 12.74], [1223424000000, 12.83], [1223510400000, 12.68], [1223596800000, 13.8], [1223856000000, 15.75], [1223942400000, 14.87], [1224028800000, 13.99], [1224115200000, 14.56], [1224201600000, 13.91], [1224460800000, 14.06], [1224547200000, 13.07], [1224633600000, 13.84], [1224720000000, 14.03], [1224806400000, 13.77], [1225065600000, 13.16], [1225152000000, 14.27], [1225238400000, 14.94], [1225324800000, 15.86], [1225411200000, 15.37], [1225670400000, 15.28], [1225756800000, 15.86], [1225843200000, 14.76], [1225929600000, 14.16], [1226016000000, 14.03], [1226275200000, 13.7], [1226361600000, 13.54], [1226448000000, 12.87], [1226534400000, 13.78], [1226620800000, 12.89], [1226880000000, 12.59], [1226966400000, 12.84], [1227052800000, 12.33], [1227139200000, 11.5], [1227225600000, 11.8], [1227484800000, 13.28], [1227571200000, 12.97], [1227657600000, 13.57], [1227830400000, 13.24], [1228089600000, 12.7], [1228176000000, 13.21], [1228262400000, 13.7], [1228348800000, 13.06], [1228435200000, 13.43], [1228694400000, 14.25], [1228780800000, 14.29], [1228867200000, 14.03], [1228953600000, 13.57], [1229040000000, 14.04], [1229299200000, 13.54]];
+        //const data = [[1220832000000, 22.56], [1220918400000, 21.67], [1221004800000, 21.66], [1221091200000, 21.81], [1221177600000, 21.28], [1221436800000, 20.05], [1221523200000, 19.98], [1221609600000, 18.26], [1221696000000, 19.16], [1221782400000, 20.13], [1222041600000, 18.72], [1222128000000, 18.12], [1222214400000, 18.39], [1222300800000, 18.85], [1222387200000, 18.32], [1222646400000, 15.04], [1222732800000, 16.24], [1222819200000, 15.59], [1222905600000, 14.3], [1222992000000, 13.87], [1223251200000, 14.02], [1223337600000, 12.74], [1223424000000, 12.83], [1223510400000, 12.68], [1223596800000, 13.8], [1223856000000, 15.75], [1223942400000, 14.87], [1224028800000, 13.99], [1224115200000, 14.56], [1224201600000, 13.91], [1224460800000, 14.06], [1224547200000, 13.07], [1224633600000, 13.84], [1224720000000, 14.03], [1224806400000, 13.77], [1225065600000, 13.16], [1225152000000, 14.27], [1225238400000, 14.94], [1225324800000, 15.86], [1225411200000, 15.37], [1225670400000, 15.28], [1225756800000, 15.86], [1225843200000, 14.76], [1225929600000, 14.16], [1226016000000, 14.03], [1226275200000, 13.7], [1226361600000, 13.54], [1226448000000, 12.87], [1226534400000, 13.78], [1226620800000, 12.89], [1226880000000, 12.59], [1226966400000, 12.84], [1227052800000, 12.33], [1227139200000, 11.5], [1227225600000, 11.8], [1227484800000, 13.28], [1227571200000, 12.97], [1227657600000, 13.57], [1227830400000, 13.24], [1228089600000, 12.7], [1228176000000, 13.21], [1228262400000, 13.7], [1228348800000, 13.06], [1228435200000, 13.43], [1228694400000, 14.25], [1228780800000, 14.29], [1228867200000, 14.03], [1228953600000, 13.57], [1229040000000, 14.04], [1229299200000, 13.54]];
+
+        const btcData = [[1448928000000, 1000], [1451606400000, 1203.34], [1454284800000, 1036.21], [1456790400000, 1203.34], [1459468800000, 1158.77], [1462060800000, 1259.05], [1464739200000, 1501.39], [1467331200000, 1880.22], [1470009600000, 1688.02],	[1472688000000, 1596.10],	[1475280000000, 1713.09],	[1477958400000, 2027.86]];
+        const portfolioData = [[1448928000000, 1000], [1451606400000, 1200], [1454284800000, 950], [1456790400000, 1250], [1459468800000, 1050], [1462060800000, 1039.5], [1464739200000, 1237.5], [1467331200000, 1287], [1470009600000, 1485], [1472688000000, 1039.5],	[1475280000000, 1089],	[1477958400000, 1237]];
+
+        var seriesOptions = [];
+
+        seriesOptions[0] = {
+            name: 'BTC',
+            data: btcData,
+            tooltip: {
+                valueDecimals: 2
+            },
+            marker: {
+                enabled: true,
+                radius: 6
+            },
+            shadow: true,
+            lineWidth: 4
+        };
+
+        seriesOptions[1] = {
+            name: 'Portfolio',
+            data: portfolioData,
+            tooltip: {
+                valueDecimals: 2
+            },
+            marker: {
+                enabled: true,
+                radius: 6
+            },
+            shadow: true,
+            lineWidth: 4
+        };
 
         const config = {
             rangeSelector: {
-                selected: 1
+                selected: 5
             },
 /*            title: {
                 text: 'BTC Price'
             },*/
-            series: [{
+            /*series: [{
                 name: 'BTC',
-                data: data,
+                data: portfolioData,
                 tooltip: {
                     valueDecimals: 2
                 }
-            }],
-            color: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
-            fillColor: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+            }],*/
+            series: seriesOptions,
+            colors: ['#00a79d', '#f8ac93'],
+            yAxis: {
+                gridLineWidth: 0,
+                //     tickColor: 'black',
+                tickLength: 5,
+                tickWidth: 1,
+                tickPosition: 'outside',
+                labels: {
+                    align: 'right',
+                    x:-10,
+                    y:5
+                },
+                lineWidth:1,
+                //  lineColor:'black'
+            }
+            //color: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+            //fillColor: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
         };
 
         return(
--- src/components/pages/dashboard/Positions.react.js
+++ src/components/pages/dashboard/Positions.react.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import PositionItem from './PositionItem.react';
 import FontAwesome from 'react-fontawesome';
-import BalanceItem from './BalanceItem.react';
 
+import './Positions.css';
 
-import './Balances.css';
-
-class Balances extends React.Component {
+class Positions extends React.Component {
     constructor(props, context) {
         super(props, context);
 
@@ -13,19 +12,19 @@ class Balances extends React.Component {
 
         setTimeout(() => {
             this.setState({loading: false});
-        }, 1500);
+        }, 1200);
     }
 
     render() {
-        const items = [
-            {title: 'BTC', amount: 12, value: '$7221.5'},
-            {title: 'ETH', amount: 20, value: '$255.44'},
-            {title: 'Augur', amount: 20, value: '$109.8'},
-            {title: 'DGD', amount: 10, value: '$121.6'}
-        ].map((item, index) => <BalanceItem key={index} item={item}/>);
 
-        return (
-            <div className="balances-list">
+        const items = [
+            {title: 'BTC', daily: '+%1.2', weekly: '+%8.65', all: '+%9938.4'},
+            {title: 'ETH', daily: '+%1.7', weekly: '+%2.5', all: '+%6538.4'},
+            {title: 'DGD', daily: '+%6', weekly: '+%12.66', all: '+%538.6'}
+        ].map((item, index) => <PositionItem key={index} item={item}/>);
+
+        return(
+            <div className="positions-cont">
                 {this.state.loading && <div className="refresh-cont"><FontAwesome ref="font" className='icon-refresh' name='refresh' size='3x' spin /></div>}
                 {!this.state.loading && items}
             </div>
@@ -33,4 +32,4 @@ class Balances extends React.Component {
     }
 }
 
-export default Balances;
+export default Positions;

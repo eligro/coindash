70bbf4e61e0198baf39300505279053cb8de3e34
Guy Aharonovsky <guy111@gmail.com>
2016-11-17 15:24:41 +0200

    charts navigation

--- src/components/pages/dashboard/DashboardPage.react.js
+++ src/components/pages/dashboard/DashboardPage.react.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './DashboardPage.css';
 import StocksChart from '../../common/charts/stocks/StocksChart.react';
+import StocksChartRisk from '../../common/charts/stocks/StocksChartRisk.react';
 import Balances from './Balances.react';
 import Positions from './Positions.react';
+import ChartNavigation from './ChartNavigation.react';
 
 class HomePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {selectedChart: 1};
+
+        this.chartSelected = this.chartSelected.bind(this);
+    }
+
+    chartSelected(selected) {
+        console.log('chartSelected', selected);
+        this.setState({selectedChart: selected});
+    }
+
     render() {
         return (
             <div className="page-container dashboard-page">
@@ -16,7 +31,9 @@ class HomePage extends React.Component {
                         <Balances />
                     </div>
                     <div className="chart-cont">
-                        <StocksChart/>
+                        <ChartNavigation handleSelectCB={this.chartSelected}/>
+                        {this.state.selectedChart === 1 && <StocksChart/>}
+                        {this.state.selectedChart === 2 && <StocksChartRisk/>}
                     </div>
                 </div>
                 <div className="bottom-cont">

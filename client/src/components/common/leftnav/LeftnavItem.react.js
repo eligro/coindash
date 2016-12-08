a6766471bbcc03fd3a7b5574b946efbb90fbd83a
Guy Aharonovsky <guy111@gmail.com>
2016-12-08 05:29:00 +0200

    active nav

--- client/src/components/common/leftnav/LeftnavItem.react.js
+++ client/src/components/common/leftnav/LeftnavItem.react.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 //import { Link } from 'react-router';
 import { browserHistory } from 'react-router';
 import FontAwesome from 'react-fontawesome';
 
 export default class LeftnavItem extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    static contextTypes = {
+        router: PropTypes.func
+    };
+
     redirectToPage() {
         browserHistory.push(this.props.item.route);
     }
 
     render() {
+        //console.log(this.context.router.isActive(this.props.item.route), this.props);
+
+        //var activeClass = this.context.router.isActive(this.props.item.route) ? 'active' : ''
+        var activeClass = this.context.router.location.pathname === this.props.item.route ? 'active' : ''
+
         return (
-            <li onClick={this.redirectToPage.bind(this)}>
+            <li onClick={this.redirectToPage.bind(this)} className={activeClass}>
                 <div className="icon-circle">
                     <FontAwesome name={this.props.item.icon} size='2x'
                                  style={this.props.item.style}/>

526979f125348fba1626ae7089d8ab27fb3c7f2d
Ilya Dorman <whitestar.i8@gmail.com>
2017-04-12 16:29:52 +0300

    balances total line added

--- client/src/components/pages/dashboard/Balances.react.js
+++ client/src/components/pages/dashboard/Balances.react.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import Spinner from '../../common/Spinner.react';
+import Utils from '../../../utils/Utils';
 import BalanceItem from './BalanceItem.react';
 
 
@@ -9,11 +10,22 @@ class Balances extends React.Component {
     render() {
         const items = this.props.balances.map((item, index) => <BalanceItem key={index} item={item}/>);
 
+        const sum = this.props.balances.reduce(function(total, value){
+            return total + value.amount;
+        }, 0);
+
+
         return (
             <div className="balances-list">
                 {!this.props.balances.length > 0 && !this.props.error && <Spinner/>}
                 {this.props.balances.length > 0 && items}
                 {this.props.error && <div>{this.props.error}</div>}
+
+                <div className="balance-item sum">
+                    <div className="item-title">Total</div>
+                    <div className="item-amount">{sum}</div>
+                    <div className="item-value" title={`$${sum}`}>${Utils.toFixed(sum, 3)}</div>
+                </div>
             </div>
         );
     }

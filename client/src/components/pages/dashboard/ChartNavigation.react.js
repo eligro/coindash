87caf1da58437b1609af3df42469512096e1def0
Eli Sklar <eli.sklar@gmail.com>
2017-04-13 12:56:07 +0300

    Standard Cleanup

--- client/src/components/pages/dashboard/ChartNavigation.react.js
+++ client/src/components/pages/dashboard/ChartNavigation.react.js
@@ -1,51 +1,49 @@
-import React from 'react';
-import {Nav, NavItem} from 'react-bootstrap';
+import React from 'react'
+import {Nav, NavItem} from 'react-bootstrap'
 
-import './ChartNavigation.css';
+import './ChartNavigation.css'
 
-import FontAwesome from 'react-fontawesome';
+import FontAwesome from 'react-fontawesome'
 
 class ChartNavigation extends React.Component {
-    constructor(props, context) {
-        super(props, context);
+  constructor (props, context) {
+    super(props, context)
 
-        this.state = {activeKey: 1};
+    this.state = {activeKey: 1}
 
-        this.handleSelect = this.handleSelect.bind(this);
-        this.handleRefresh = this.handleRefresh.bind(this);
-    }
+    this.handleSelect = this.handleSelect.bind(this)
+    this.handleRefresh = this.handleRefresh.bind(this)
+  }
 
+  handleSelect (selectedKey) {
+    this.setState({activeKey: selectedKey})
 
+    this.props.handleSelectCB(selectedKey)
+  }
 
-    handleSelect(selectedKey) {
-        this.setState({activeKey: selectedKey});
+  handleRefresh () {
+    this.props.handleRefreshCB()
+  }
 
-        this.props.handleSelectCB(selectedKey);
-    }
+  render () {
+    return (
+      <div className='chart-navigation'>
+        <div className='status-cont'>
+          <p className='title'> {this.props.statusText} </p>
+        </div>
+        <div className='btn-cont'>
+          <button className='btn' onClick={this.handleRefresh}>
+            <FontAwesome name='refresh' size='2x' />
+          </button>
+        </div>
+        <Nav className='navbar-cont' bsStyle='pills' activeKey={this.state.activeKey} onSelect={(selectedKey) => { this.handleSelect(selectedKey) }}>
+          <NavItem eventKey={1} title='item1'>Performance</NavItem>
+          <NavItem eventKey={2} title='Item2'>Risk</NavItem>
+        </Nav>
 
-    handleRefresh() {
-        this.props.handleRefreshCB();
-    }
-        
-    render() {
-        return (
-            <div className="chart-navigation">
-                <div className="status-cont">
-                    <p className="title"> {this.props.statusText} </p>
-                </div>
-                <div className="btn-cont">
-                    <button className="btn" onClick={this.handleRefresh}>
-                        <FontAwesome name="refresh" size='2x'/>
-                    </button>
-                </div>
-                <Nav className="navbar-cont" bsStyle="pills" activeKey={this.state.activeKey} onSelect={(selectedKey) => { this.handleSelect(selectedKey);}}>
-                    <NavItem eventKey={1} title="item1">Performance</NavItem>
-                    <NavItem eventKey={2} title="Item2">Risk</NavItem>
-                </Nav>
-                
-            </div>
-        );
-    }
+      </div>
+    )
+  }
 }
 
-export default ChartNavigation;
+export default ChartNavigation

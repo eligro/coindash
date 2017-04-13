003f5eb51f3d055289a9dd28f931bb986ac8ee99
Eli Sklar <eli.sklar@gmail.com>
2017-04-13 12:07:24 +0300

    balance components cleanup

--- client/src/components/pages/dashboard/Balances.react.js
+++ client/src/components/pages/dashboard/Balances.react.js
@@ -1,40 +1,36 @@
-import React, {PropTypes} from 'react';
-import Spinner from '../../common/Spinner.react';
-import Utils from '../../../utils/Utils';
-import BalanceItem from './BalanceItem.react';
+import React, {PropTypes} from 'react'
+import Spinner from '../../common/Spinner.react'
+import Utils from '../../../utils/Utils'
+import BalanceItem from './BalanceItem.react'
 
-
-import './Balances.css';
+import './Balances.css'
 
 class Balances extends React.Component {
-    render() {
-        const items = this.props.balances.map((item, index) => <BalanceItem key={index} item={item}/>);
+  render () {
+    const items = this.props.balances.map((item, index) => <BalanceItem key={index} item={item} />)
 
-            console.log(this.props.balances);
+    const sum = this.props.balances.reduce(function (total, value) {
+      return total + value.value
+    }, 0)
 
-        const sum = this.props.balances.reduce(function(total, value){
-            return total + value.value;
-        }, 0);
+    return (
+      <div className='balances-list'>
+        {!this.props.balances.length > 0 && !this.props.error && <Spinner />}
+        {this.props.balances.length > 0 && items}
+        {this.props.error && <div>{this.props.error}</div>}
 
-
-        return (
-            <div className="balances-list">
-                {!this.props.balances.length > 0 && !this.props.error && <Spinner/>}
-                {this.props.balances.length > 0 && items}
-                {this.props.error && <div>{this.props.error}</div>}
-
-                <div className="balance-item sum">
-                    <div className="item-title">Total</div>
-                    <div className="item-amount"></div>
-                    <div className="item-value" title={`$${sum}`}>${Utils.toFixed(sum, 3)}</div>
-                </div>
-            </div>
-        );
-    }
+        <div className='balance-item sum'>
+          <div className='item-title'>Total</div>
+          <div className='item-amount' />
+          <div className='item-value' title={`$${sum}`}>${Utils.toFixed(sum, 3)}</div>
+        </div>
+      </div>
+    )
+  }
 }
 
 Balances.propTypes = {
-    balances: PropTypes.array.isRequired
+  balances: PropTypes.array.isRequired
 }
 
-export default Balances;
+export default Balances

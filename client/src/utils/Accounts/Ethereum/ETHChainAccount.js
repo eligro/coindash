540e4f4df8427de932ee0ffb6030bedd6eb84f35
Eli Sklar <eli.sklar@gmail.com>
2017-04-13 15:08:28 +0300

    `ETHChainAccount.js` cleanup

--- client/src/utils/Accounts/Ethereum/ETHChainAccount.js
+++ client/src/utils/Accounts/Ethereum/ETHChainAccount.js
@@ -1,306 +1,297 @@
-import { Account } from '../Account.js';
-import { Token } from '../../Trades/Token';
-import { Trade } from '../../Trades/Trade';
-import { WithdrawlDeposit } from '../../Trades/WithdrawlDeposit';
-import { ETHTransaction, ERC20Data } from './ETHTransaction';
-import { AjaxUtils } from '../../utils/AjaxUtils';
-import { Utils } from '../../utils/Utils';
-import BigNumber from 'bignumber.js';
-import { ETHHelper } from './ETHHelper';
+import { Account } from '../Account.js'
+import { Token } from '../../Trades/Token'
+import { Trade } from '../../Trades/Trade'
+import { WithdrawlDeposit } from '../../Trades/WithdrawlDeposit'
+import { ETHTransaction, ERC20Data } from './ETHTransaction'
+import { AjaxUtils } from '../../utils/AjaxUtils'
+import { Utils } from '../../utils/Utils'
+import BigNumber from 'bignumber.js'
+import { ETHHelper } from './ETHHelper'
 
 export class ETHChainAccount extends Account {
-	constructor(ethAccount, watchedTokens) {
-		super("Ethereum_" + ethAccount);
+  constructor (ethAccount, watchedTokens) {
+    super('Ethereum_' + ethAccount)
 
-		this.ethAccount = ethAccount;
+    this.ethAccount = ethAccount
 
-		this.watchedTokens = watchedTokens;
-		
-		// attach accoutn address
-		for (var i = 0; i < this.watchedTokens.length; i++) {
-	      let t = this.watchedTokens[i];
-	      t.userAddress = this.ethAccount;
-	    }
-	}
+    this.watchedTokens = watchedTokens
 
-	getTrades(callback) {
-		this.fetchICOBuyinTrades(this.ethAccount, callback);
-	}
+    // attach accoutn address
+    for (var i = 0; i < this.watchedTokens.length; i++) {
+        let t = this.watchedTokens[i]
+        t.userAddress = this.ethAccount
+      }
+  }
 
-	getBalances(callback) {
-		this.fetchAllBalances(this.watchedTokens, 0, callback);
-	}
+  getTrades (callback) {
+    this.fetchICOBuyinTrades(this.ethAccount, callback)
+  }
 
-	getTransactionHistory(callback) {
-		this.fetchTxHistoryAndTokenHistory(this.ethAccount, function(txs) {
-			callback(txs);
-		});
-	}
+  getBalances (callback) {
+    this.fetchAllBalances(this.watchedTokens, 0, callback)
+  }
 
-	getWithdrawAndDepositHistory(callback) {
-		let parentObj = this;
-		this.getTransactionHistory(function(txs) {
-			let ret = [];
-			for(let idx in txs) {
-				let tx = txs[idx];
-				
-				tx.tokenTransaction = parentObj.findTokenICOTrade(tx);
-				let erc20Data = tx.getERC20Data();
-				if (erc20Data != null) { // ERC 20 action
-					let d = erc20Data.getDepositWithdrawl(parentObj.ethAccount); // only ERC20 transfer req is returned
-					if (d != null) {
-						d.account = parentObj;
-						ret.push(d);
-					}
-				}
-				else if (tx.value > 0) { // value transfer
-					if (tx.from === parentObj.ethAccount) { // withdrawl
-						ret.push(new WithdrawlDeposit(
-							WithdrawlDeposit.Types().Withdrawl,
-							parseInt(tx.timeStamp),
-							Token.ETH(),
-							tx.weiBalance(),
-							tx.to,
-							parentObj,
-							tx
-						))
-					}
-					else { // deposit
-						ret.push(new WithdrawlDeposit(
-							WithdrawlDeposit.Types().Deposit,
-							parseInt(tx.timeStamp),
-							Token.ETH(),
-							tx.weiBalance(),
-							tx.from,
-							parentObj,
-							tx
-						))
-					}
-				}
-			}
-			callback(ret);
-		});
-	}
+  getTransactionHistory (callback) {
+    this.fetchTxHistoryAndTokenHistory(this.ethAccount, function (txs) {
+      callback(txs)
+    })
+  }
 
-	// private
-	fetchAllBalances(tokens, tokenIdx, callback) {
+  getWithdrawAndDepositHistory (callback) {
+    let parentObj = this
+    this.getTransactionHistory(function (txs) {
+      let ret = []
+      for (let idx in txs) {
+        let tx = txs[idx]
 
-		let cntActions = 0;
-		for (let i = 0; i < tokens.length; i++) {
-			cntActions ++;
+        tx.tokenTransaction = parentObj.findTokenICOTrade(tx)
+        let erc20Data = tx.getERC20Data()
+        if (erc20Data != null) { // ERC 20 action
+          let d = erc20Data.getDepositWithdrawl(parentObj.ethAccount) // only ERC20 transfer req is returned
+          if (d != null) {
+            d.account = parentObj
+            ret.push(d)
+          }
+        } else if (tx.value > 0) { // value transfer
+          if (tx.from === parentObj.ethAccount) { // withdrawl
+            ret.push(new WithdrawlDeposit(
+              WithdrawlDeposit.Types().Withdrawl,
+              parseInt(tx.timeStamp),
+              Token.ETH(),
+              tx.weiBalance(),
+              tx.to,
+              parentObj,
+              tx
+            ))
+          } else { // deposit
+            ret.push(new WithdrawlDeposit(
+              WithdrawlDeposit.Types().Deposit,
+              parseInt(tx.timeStamp),
+              Token.ETH(),
+              tx.weiBalance(),
+              tx.from,
+              parentObj,
+              tx
+            ))
+          }
+        }
+      }
+      callback(ret)
+    })
+  }
 
-			let token = tokens[i];
+  // private
+  fetchAllBalances (tokens, tokenIdx, callback) {
+    let cntActions = 0
+    for (let i = 0; i < tokens.length; i++) {
+      cntActions++
 
-			this.fetchBalanceForToken(token, function (t, balance) {
-				t.balance = balance;
-				
-				cntActions --;
-				if (cntActions == 0) {
-					callback(tokens);
-				}
-			});
-		}
-	}
+      let token = tokens[i]
 
-	fetchBalanceForToken(token, callback) {
-	    ETHHelper.fetchBalanceForToken(token, this.ethAccount, callback);
-	}
+      this.fetchBalanceForToken(token, function (t, balance) {
+        t.balance = balance
 
-	// old MEW api
+        cntActions--
+        if (cntActions == 0) {
+          callback(tokens)
+        }
+      })
+    }
+  }
 
-	// fetchBalanceForToken(token, callback) {
-	// 	let data = AjaxUtils.queryParams(token.balanceCallData());
-	//     let serverUrl = "https://rpc.myetherwallet.com/api.mew";
-	//     let parentObj = this;
+  fetchBalanceForToken (token, callback) {
+      ETHHelper.fetchBalanceForToken(token, this.ethAccount, callback)
+  }
 
-	//     fetch(serverUrl, {
-	//       method: 'post',
-	//       headers: {
-	//         'Content-Type': 'application/x-www-form-urlencoded',
-	//       },
-	//       body: data
-	//     })
-	//     .then((response) => response.json())
-	//     .then((data) => {
-	//       if (!data.error) {
-	//         if (token.symbol === "ETH" || token.symbol === "ETC") {
-	//           token.balance = data.data.balance;
-	//           token.balance = new BigNumber(token.weiBalance());
-	//           callback(token.balance);
-	//         }
-	//         else {
-	//           let decimal = token.decimal;
-	//           let _balance = new BigNumber(data.data).div(new BigNumber(10).pow(decimal));
-	//           token.balance = _balance;
-	//           callback(_balance);
-	//         }
-	//       }
-	//       else {
-	//       	callback(new BigNumber(0));
-	//       }
-	//     })
-	//     .catch((error) => {
-	//       console.error(error);
-	//     });
-	// }
+  // old MEW api
 
-	findTokenICOTrade(tx) {
-		let allTokens = this.watchedTokens;
-		for(let idx in allTokens) {
-			let tokenAddress = allTokens[idx].contractAddress.replace('0x','');
-			let tokenICOAddress = "";
-			if (allTokens[idx].ico_address) {
-				tokenICOAddress = allTokens[idx].ico_address.replace('0x','');
-			}
+  // fetchBalanceForToken(token, callback) {
+  //   let data = AjaxUtils.queryParams(token.balanceCallData());
+  //     let serverUrl = "https://rpc.myetherwallet.com/api.mew";
+  //     let parentObj = this;
 
-			let txTo = tx.to.replace('0x','');
+  //     fetch(serverUrl, {
+  //       method: 'post',
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //       },
+  //       body: data
+  //     })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (!data.error) {
+  //         if (token.symbol === "ETH" || token.symbol === "ETC") {
+  //           token.balance = data.data.balance;
+  //           token.balance = new BigNumber(token.weiBalance());
+  //           callback(token.balance);
+  //         }
+  //         else {
+  //           let decimal = token.decimal;
+  //           let _balance = new BigNumber(data.data).div(new BigNumber(10).pow(decimal));
+  //           token.balance = _balance;
+  //           callback(_balance);
+  //         }
+  //       }
+  //       else {
+  //         callback(new BigNumber(0));
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }
 
-			if(tokenAddress.toLowerCase() === txTo.toLowerCase() || 
-				tokenICOAddress.toLowerCase() === txTo.toLowerCase()) {
-				return allTokens[idx];
-			}
-		}
-		return null;
-	}
+  findTokenICOTrade (tx) {
+    let allTokens = this.watchedTokens
+    for (let idx in allTokens) {
+      let tokenAddress = allTokens[idx].contractAddress.replace('0x', '')
+      let tokenICOAddress = ''
+      if (allTokens[idx].ico_address) {
+        tokenICOAddress = allTokens[idx].ico_address.replace('0x', '')
+      }
 
-	fetchICOBuyinTrades(account, callback) {
-		let parentObj = this;
-		this.fetchTxsForAccount(account, function(txs, error) {
-			if(error != null) {
-				callback(null, error);
-			}
-			else {
-				let _ret = [];
-				for(let idx in txs) {
-					let tx = txs[idx];
-					tx.tokenTransaction = parentObj.findTokenICOTrade(tx);
-					let erc20Data = tx.getERC20Data();
-					if (erc20Data != null) {
-						// buyin
-						if (erc20Data.type === ERC20Data.OperationType().Buyin) {
-							_ret.push(erc20Data.getTrade());
-						}
+      let txTo = tx.to.replace('0x', '')
 
-						// transfer
-						// else if (erc20Data.type === ERC20Data.OperationType().Transfer) {
-						// 	console.log(erc20Data.timestamp + ") Transfered " + erc20Data.value + " from " + tx.tokenTransaction.prettyName());
-						// }
-					}
-				}
+      if (tokenAddress.toLowerCase() === txTo.toLowerCase() ||
+        tokenICOAddress.toLowerCase() === txTo.toLowerCase()) {
+        return allTokens[idx]
+      }
+    }
+    return null
+  }
 
-				callback(_ret, null);
-			}
-		});
-	}
+  fetchICOBuyinTrades (account, callback) {
+    let parentObj = this
+    this.fetchTxsForAccount(account, function (txs, error) {
+      if (error != null) {
+        callback(null, error)
+      } else {
+        let _ret = []
+        for (let idx in txs) {
+          let tx = txs[idx]
+          tx.tokenTransaction = parentObj.findTokenICOTrade(tx)
+          let erc20Data = tx.getERC20Data()
+          if (erc20Data != null) {
+            // buyin
+            if (erc20Data.type === ERC20Data.OperationType().Buyin) {
+              _ret.push(erc20Data.getTrade())
+            }
 
-	fetchTxsForAccount(account, callback) {
-		let prefix = "http://api.etherscan.io/api?module=account&action=txlist&address="
-		let suffix = "&startblock=0&endblock=99999999&sort=asc&apikey=38DE12F4P7CNASZBM3RRAEWPHJKMWQD2NU";
-		let serverUrl = prefix + account + suffix;
+            // transfer
+            // else if (erc20Data.type === ERC20Data.OperationType().Transfer) {
+            //   console.log(erc20Data.timestamp + ") Transfered " + erc20Data.value + " from " + tx.tokenTransaction.prettyName());
+            // }
+          }
+        }
 
-	    fetch(serverUrl, {
-	      method: 'get',
-	    })
-	    .then((response) => response.json())
-	    .then((data) => {
-	      if (data.status === "1") {
-	      	let _txs = [];
-	      	for (let idx in data.result) {
-	      		if (data.result[idx].isError === "0") {
-		      		_txs.push(ETHTransaction.fromEtherscanDic(data.result[idx]));
-	      		}
-	      	}
-	      	callback(_txs);
-	      }
-	      else {
-	      	// handle error
-	      	let _txs = [];
-	      	callback(_txs);
-	      }
-	    })
-	    .catch((error) => {
-	      console.error(error);
-	      // alert("Some problems with the account, try again later");
-	    });
-	}
+        callback(_ret, null)
+      }
+    })
+  }
 
-	fetchTxHistoryAndTokenHistory(account, callback) {
-		let parentObj = this;
+  fetchTxsForAccount (account, callback) {
+    let prefix = 'http://api.etherscan.io/api?module=account&action=txlist&address='
+    let suffix = '&startblock=0&endblock=99999999&sort=asc&apikey=38DE12F4P7CNASZBM3RRAEWPHJKMWQD2NU'
+    let serverUrl = prefix + account + suffix
 
-		this.fetchTxsForAccount(account, function(_txs) {
+      fetch(serverUrl, {
+        method: 'get'
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === '1') {
+          let _txs = []
+          for (let idx in data.result) {
+            if (data.result[idx].isError === '0') {
+              _txs.push(ETHTransaction.fromEtherscanDic(data.result[idx]))
+            }
+          }
+          callback(_txs)
+        } else {
+          // handle error
+          let _txs = []
+          callback(_txs)
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+        // alert("Some problems with the account, try again later");
+      })
+  }
 
-			// fetch token contract txs
-	      	parentObj.fetchAllTokenContractTxList(0, parentObj.watchedTokens, [], account, function(txsLst) {
-		    	txsLst.push(_txs);
-		    	callback(Utils.concatArrayOfArrays(txsLst));
-		    });
+  fetchTxHistoryAndTokenHistory (account, callback) {
+    let parentObj = this
 
-		})
-	}
+    this.fetchTxsForAccount(account, function (_txs) {
+      // fetch token contract txs
+          parentObj.fetchAllTokenContractTxList(0, parentObj.watchedTokens, [], account, function (txsLst) {
+          txsLst.push(_txs)
+          callback(Utils.concatArrayOfArrays(txsLst))
+        })
+    })
+  }
 
-	fetchAllTokenContractTxList(idx, tokens, balances, account, callback) {
+  fetchAllTokenContractTxList (idx, tokens, balances, account, callback) {
+    let cntActions = 0
+    for (let i = 0; i < tokens.length; i++) {
+      cntActions++
 
-		let cntActions = 0;
-		for (let i = 0; i < tokens.length; i++) {
-			cntActions ++;
+      let t = tokens[i]
 
-			let t = tokens[i];
+      this.fetchTokenContractTxList(t, account, function (txs) {
+        balances.push(txs)
 
-			this.fetchTokenContractTxList(t, account, function(txs) {
-				balances.push(txs);
+        cntActions--
+        if (cntActions == 0) {
+          callback(balances)
+        }
+      })
+    }
+  }
 
-				cntActions --;
-				if (cntActions == 0) {
-					callback(balances);
-				}
-			});
-		}
-	}
+  fetchTokenContractTxList (token, account, callback) {
+    if (token.contractAddress == null || token.contractAddress.length == 0) {
+      callback([])
+      return
+    }
 
-	fetchTokenContractTxList(token, account, callback) {
-		if (token.contractAddress == null || token.contractAddress.length == 0) {
-			callback([]);
-			return 
-		}
+    let prefix = 'http://api.etherscan.io/api?module=account&action=txlist&address='
+    let suffix = '&startblock=0&endblock=99999999&sort=asc&apikey=38DE12F4P7CNASZBM3RRAEWPHJKMWQD2NU'
+    let serverUrl = prefix + token.contractAddress + suffix
 
-		let prefix = "http://api.etherscan.io/api?module=account&action=txlist&address="
-		let suffix = "&startblock=0&endblock=99999999&sort=asc&apikey=38DE12F4P7CNASZBM3RRAEWPHJKMWQD2NU";
-		let serverUrl = prefix + token.contractAddress + suffix;
+    let parentObj = this
 
-		let parentObj = this;
+    fetch(serverUrl, {
+        method: 'get'
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === '1') {
+          let _txs = []
 
-		fetch(serverUrl, {
-	      method: 'get',
-	    })
-	    .then((response) => response.json())
-	    .then((data) => {
-	      if (data.status === "1") {
-	      	let _txs = [];
+          for (let idx in data.result) {
+            if (data.result[idx].isError === '0') {
+              let tx = ETHTransaction.fromEtherscanDic(data.result[idx])
+              tx.tokenTransaction = parentObj.findTokenICOTrade(tx)
+              let erc20Data = tx.getERC20Data()
 
-	      	for (let idx in data.result) {
-	      		if (data.result[idx].isError === "0") {
-		      		let tx = ETHTransaction.fromEtherscanDic(data.result[idx]);
-		      		tx.tokenTransaction = parentObj.findTokenICOTrade(tx);
-		      		let erc20Data = tx.getERC20Data();
+        if (erc20Data != null &&
+            erc20Data.type === ERC20Data.OperationType().Transfer &&
+            erc20Data.account.toLowerCase() === account.toLowerCase()) {
+          _txs.push(tx)
+        }
+            }
+          }
 
-					if (erc20Data != null && 
-						erc20Data.type === ERC20Data.OperationType().Transfer &&
-						erc20Data.account.toLowerCase() === account.toLowerCase()) {
-						_txs.push(tx);
-					}
-	      		}
-	      	}
-
-	      	callback(_txs);
-	      }
-	      else {
-	      	// handle error
-	      	console.log(data);
-	      }
-	    })
-	    .catch((error) => {
-	      console.error(error);
-	      // alert("Some problems with the account, try again later");
-	    });
-	}
+          callback(_txs)
+        } else {
+          // handle error
+          console.log(data)
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+        // alert("Some problems with the account, try again later");
+      })
+  }
 }

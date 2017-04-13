7fb549c5d4e375910b0783399b978e39822ed2e8
Eli Sklar <eli.sklar@gmail.com>
2017-04-13 12:06:36 +0300

    Fixes balances align issues

--- client/src/components/pages/dashboard/BalanceItem.react.js
+++ client/src/components/pages/dashboard/BalanceItem.react.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Utils from '../../../utils/Utils';
 
+const toFixed = (num, len = 2) => parseFloat(Math.round(num * 100) / 100).toFixed(len)
+
 class BalanceItem extends React.Component {
     render() {
         return(
             <div className="balance-item">
                 <div className="item-title">{this.props.item.title}</div>
-                <div className="item-amount">{this.props.item.amount}</div>
+                <div className="item-amount">{toFixed(this.props.item.amount, 4)}</div>
                 <div className="item-value" title={`$${this.props.item.value}`}>${Utils.toFixed(this.props.item.value, 3)}</div>
             </div>
         );

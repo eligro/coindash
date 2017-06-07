59cb93295df63bd4c6678e9fc4e1fd71059e0af5
Eli Sklar <eli.sklar@gmail.com>
2017-06-07 15:51:08 +0300

    - more details for user profile - added reset password route - create `no auth page` component

--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -6,7 +6,8 @@ export default function userReducer (state = {}, action) {
       return Object.assign({}, state, {
         loggedIn: true,
         email: action.data.email,
-        error: false
+        error: false,
+        profile: action.data
       })
 
     case types.LOGIN_FAILED:

80f19e7a2ac1647680cbd5a3611b5b9a6e8e6325
Eli Sklar <eli.sklar@gmail.com>
2017-06-10 04:10:23 +0300

    console cleanup

--- src/reducers/admin.reducer.js
+++ src/reducers/admin.reducer.js
@@ -3,7 +3,6 @@ import * as types from '../actions/action.const'
 export default function userReducer (state = {}, action) {
   switch (action.type) {
     case types.ADMIN_ON:
-      console.log('reducer ADMIN ON', action)
       return Object.assign({}, state, {
         active: true
       })

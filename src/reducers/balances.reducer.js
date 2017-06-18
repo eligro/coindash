bb1a86cdd304912cae07d11214d70821014478fe
Eli Sklar <eli.sklar@gmail.com>
2017-06-18 15:36:07 +0300

    balances fetched, stored in firebase, and displayed on portfolio

--- src/reducers/balances.reducer.js
+++ src/reducers/balances.reducer.js
@@ -1,10 +1,76 @@
 import * as types from '../actions/action.const'
+import * as persist from 'redux-persist/constants'
 
-export default function balancesReducer (state = [], action) {
+const baseState = {
+  byPid: {}
+}
+
+export default function balancesReducer (state = {...baseState}, {pid, data, error, ...action}) {
+  let pState = (pid && state.byPid && state.byPid[pid]) || {}
+  let balances = []
   switch (action.type) {
     case types.LOAD_BALANCES_SUCCESS:
       return [...action.data]
 
+    case types.CALC_BALANCES_FETCH:
+      return Object.assign({}, state, {
+        byPid: {
+          ...state.byPid,
+          [pid]: {
+            ...pState,
+            fetching: true
+          }
+        }
+      })
+    case types.CALC_BALANCES_START:
+      return Object.assign({}, state, {
+        byPid: {
+          ...state.byPid,
+          [pid]: {
+            ...pState,
+            started: true,
+            fetching: false
+          }
+        }
+      })
+    case types.CALC_BALANCES_SUCCESS:
+      return Object.assign({}, state, {
+        byPid: {
+          ...state.byPid,
+          [pid]: {
+            ...pState,
+            fetching: false,
+            completed: true,
+            data: data.data.map(e => ({...e})) || [],
+            updatedOn: data.updatedOn
+          }
+        }
+      })
+
+    case types.CALC_BALANCES_ADD:
+      return Object.assign({}, state, {
+        byPid: {
+          ...state.byPid,
+          [pid]: {
+            ...pState,
+            fetching: false,
+            data: data.data.map(e => ({...e})) || [],
+            updatedOn: data.updatedOn
+          }
+        }
+      })
+    case types.CALC_BALANCES_ERROR:
+      return Object.assign({}, state, {
+        byPid: {
+          ...state.byPid,
+          [pid]: {
+            ...pState,
+            error
+          }
+        }
+      })
+
+    case persist.REHYDRATE:
     case types.CLEAR_BALANCES:
       return []
 
